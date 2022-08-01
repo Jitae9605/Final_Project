291c656868af4fdc0d24e756e4ffb684c39997de
Jitae9605 <dlwlxo3819@naver.com>
2022-08-01 15:51:03 +0900

    MVVM 패턴 템플릿(참고용) 작성완료

--- WPF_SmartFarmMonitoringSystem/Models/Member.cs
+++ WPF_SmartFarmMonitoringSystem/Models/Member.cs
@@ -4,6 +4,15 @@ namespace WPF_SmartFarmMonitoringSystem.Models
 {
 	public class Member
 	{
-		
+		public string Name;
+		public string Tel;
+		public string Message;
+
+		public Member(string InName, string InTel, string InMessage)
+		{
+			Name = InName;
+			Tel = InTel;
+			Message = InMessage;
+		}
 	}
 }
--- WPF_SmartFarmMonitoringSystem/ViewModels/MainViewModel.cs
+++ WPF_SmartFarmMonitoringSystem/ViewModels/MainViewModel.cs
@@ -5,6 +5,8 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 using WPF_SmartFarmMonitoringSystem.Bases;
 using WPF_SmartFarmMonitoringSystem.Models;
 
@@ -16,5 +18,103 @@ namespace WPF_SmartFarmMonitoringSystem.ViewModels
 	public class MainViewModel : ViewModelBase
 	{
 
+		private string inName;
+		public string InName 
+		{ 
+			get => inName;
+			set
+			{
+				inName = value;
+				RaisePropertyChanged("InName"); // 값이 바뀌었음을 공지
+			}
+		}
+
+		private string inTel;
+		public string InTel
+		{
+			get => inTel;
+			set
+			{
+				inTel = value;
+				RaisePropertyChanged("InTel"); // 값이 바뀌었음을 공지
+			}
+		}
+
+		private string inMessage;
+		public string InMessage
+		{
+			get => inMessage;
+			set
+			{
+				inMessage = value;
+				RaisePropertyChanged("InMessage"); // 값이 바뀌었음을 공지
+			}
+		}
+
+		private string outName;
+		public string OutName
+		{
+			get => outName;
+			set
+			{
+				outName = value;
+				RaisePropertyChanged("OutName"); // 값이 바뀌었음을 공지
+			}
+		}
+
+		private string outTel;
+		public string OutTel
+		{
+			get => outTel;
+			set
+			{
+				outTel = value;
+				RaisePropertyChanged("OutTel"); // 값이 바뀌었음을 공지
+			}
+		}
+
+		private string outMessage;
+		public string OutMessage
+		{
+			get => outMessage;
+			set
+			{
+				outMessage = value;
+				RaisePropertyChanged("OutMessage"); // 값이 바뀌었음을 공지
+			}
+		}
+
+		// 값이 전부 적용되서 버튼을 활성화 하기위한 명령
+		private ICommand proceedCommand;
+		public ICommand ProceedCommand
+		{
+			get
+			{
+				return proceedCommand ?? (
+				  proceedCommand = new RelayCommand<object>(
+					  o => Proceed(), o => !string.IsNullOrEmpty(InName) &&
+										  !string.IsNullOrEmpty(InTel) &&
+										  !string.IsNullOrEmpty(InMessage)
+				  ));
+			}
+		}
+
+		// 버튼클릭시 일어나는 실제 명령의 실체S
+		private async void Proceed()
+		{
+			try
+			{
+				//DateTime date = inDate ?? DateTime.Now;
+				Member person = new Member(InName, InTel, InMessage);
+
+				await Task.Run(() => OutName = person.Name);
+				await Task.Run(() => OutTel = person.Tel);
+				await Task.Run(() => OutMessage = person.Message);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show($"예외발생 : {ex.Message}");
+			}
+		}
 	}
 }
