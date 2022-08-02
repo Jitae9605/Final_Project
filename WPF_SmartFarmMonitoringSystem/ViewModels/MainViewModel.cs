339000c3d539099d3d51899110fa802efabf3530
Jitae9605 <dlwlxo3819@naver.com>
2022-08-02 12:14:42 +0900

    프로젝트 템플릿 생성 및 초안 작성

--- WPF_SmartFarmMonitoringSystem/ViewModels/MainViewModel.cs
+++ WPF_SmartFarmMonitoringSystem/ViewModels/MainViewModel.cs
@@ -1,158 +1,85 @@
-﻿using Microsoft.Toolkit.Mvvm.Input;
-using Newtonsoft.Json.Linq;
+﻿using Caliburn.Micro;
 using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Input;
-using WPF_SmartFarmMonitoringSystem.Bases;
-using WPF_SmartFarmMonitoringSystem.Models;
+using WPF_SmartFarmMonitoringSystem.Helpers;
 
 namespace WPF_SmartFarmMonitoringSystem.ViewModels
 {
-	/// <summary>
-	/// 메인 뷰모델 클래스
-	/// </summary>
-	public class MainViewModel : ViewModelBase
+	public class MainViewModel : Conductor<object> // Screen에는 ActivateItem[Async] 메서드 없음!
 	{
-
-		private string inName;
-		public string InName
+		protected override Task OnDeactivateAsync(bool close, CancellationToken cancellationToken)
 		{
-			get => inName;
-			set
+			if(Commons.MQTT_CLINET.IsConnected)
 			{
-				inName = value;
-				RaisePropertyChanged("InName"); // 값이 바뀌었음을 공지
-			}
+				Commons.MQTT_CLINET.Disconnect();
+				Commons.MQTT_CLINET = null;
+			} // 비활성화 처리
+
+			return base.OnDeactivateAsync(close, cancellationToken);
+		}
+		public MainViewModel()
+		{
+			DisplayName = "SmartHome Mpnitoring v2.0";  // 프로그램 타이틀, 이름
 		}
 
-		private string inTel;
-		public string InTel
+		public void LoadDataBaseView()
 		{
-			get => inTel;
-			set
-			{
-				inTel = value;
-				RaisePropertyChanged("InTel"); // 값이 바뀌었음을 공지
-			}
+			ActivateItemAsync(new DataBaseViewModel());
 		}
 
-		private string inMessage;
-		public string InMessage
+		public void LoadRealTimeView()
 		{
-			get => inMessage;
-			set
-			{
-				inMessage = value;
-				RaisePropertyChanged("InMessage"); // 값이 바뀌었음을 공지
-			}
+			ActivateItemAsync(new RealTimeViewModel());
 		}
 
-		private string outName;
-		public string OutName
+		public void LoadHistoryView()
 		{
-			get => outName;
-			set
-			{
-				outName = value;
-				RaisePropertyChanged("OutName"); // 값이 바뀌었음을 공지
-			}
+			ActivateItemAsync(new HistoryViewModel());
 		}
 
-		private string outTel;
-		public string OutTel
+		public void Exit_Menu()
 		{
-			get => outTel;
-			set
-			{
-				outTel = value;
-				RaisePropertyChanged("OutTel"); // 값이 바뀌었음을 공지
-			}
+			ExitProgram();
+		}
+		public void Exit_Toolbar()
+		{
+			ExitProgram();
 		}
 
-		private string outMessage;
-		public string OutMessage
+		private void ExitProgram()
 		{
-			get => outMessage;
-			set
-			{
-				outMessage = value;
-				RaisePropertyChanged("OutMessage"); // 값이 바뀌었음을 공지
-			}
-		}
-
-		// 값이 전부 적용되서 버튼을 활성화 하기위한 명령
-		private ICommand proceedCommand;
-		public ICommand ProceedCommand
-		{
-			get
-			{
-				return proceedCommand ?? (
-				  proceedCommand = new RelayCommand<object>(
-					  o => Proceed(), o => !string.IsNullOrEmpty(InName) &&
-										  !string.IsNullOrEmpty(InTel) &&
-										  !string.IsNullOrEmpty(InMessage)
-				  ));
-			}
-		}
-
-		// 버튼클릭시 일어나는 실제 명령의 실체S
-		private async void Proceed()
-		{
-			try
-			{
-				//DateTime date = inDate ?? DateTime.Now;
-				Member person = new Member(InName, InTel, InMessage);
-
-				await Task.Run(() => OutName = person.Name);
-				await Task.Run(() => OutTel = person.Tel);
-				await Task.Run(() => OutMessage = person.Message);
-				Program();
-			}
-			catch (Exception ex)
-			{
-				MessageBox.Show($"예외발생 : {ex.Message}");
-			}
+			Environment.Exit(0); // 프로그램 종료
 		}
 
 
-		// 공공데이터포탈 초단기예보
-		public void Program()
+		// Start메뉴, 아이콘 눌렀을때 처리할 이벤트
+		public void PopInfoDialog()
 		{
-			HttpClient client = new HttpClient();
+			TaskPopup();
+		}
 
-			string url = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst"; // URL
-			url += "?ServiceKey=" + "4YCQTPPRPGl%2BKP4xb5gVtRyCx%2BjzC7gpfKXt1k%2FHbp3sRxvB9K3P14K52GvE5XuouMRmGCgfZnRaVhQNR%2Bz9kg%3D%3D"; // Service Key
-			url += "&pageNo=1";
-			url += "&numOfRows=1000";
-			url += "&dataType=JSON";
-			url += "&base_date=20220801";
-			url += "&base_time=0000";
-			url += "&nx=55";
-			url += "&ny=127";
+		public void StartSubscribe()
+		{
+			TaskPopup();
+		}
 
-			var request = (HttpWebRequest)WebRequest.Create(url);
-			request.Method = "GET";
+		private void TaskPopup()
+		{
+			//CustomPopupView
+			var winManger = new WindowManager();
+			var result = winManger.ShowDialogAsync(new CustomPopupViewModel("New Broker"));
 
-			string results = string.Empty;
-			HttpWebResponse response;
-			using (response = request.GetResponse() as HttpWebResponse)
+			if (result.Result == true)
 			{
-				StreamReader reader = new StreamReader(response.GetResponseStream());
-				results = reader.ReadToEnd();
+				ActivateItemAsync(new DataBaseViewModel());
 			}
+		}
 
-			
-
-
-			MessageBox.Show(results);
+		public void PopInfoView()
+		{
+			var winManger = new WindowManager();
+			winManger.ShowDialogAsync(new CustomInfoViewModel("About"));
 		}
 	}
 }
